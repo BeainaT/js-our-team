83b340eae3c43725957a590975192e3167f5d6d1
BeainaT <tania.df@hotmail.it>
2022-05-14 14:36:50 +0200

    added bonus

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
 
 //Definisco array popolato da oggetti, ogni oggetto definisce un membro del team.
-const members = [
+let members = [
     {
         memberName: "Wayne Barnett",
         role: "Founder & CEO",
@@ -38,14 +38,13 @@ const members = [
 const mainContainer = document.querySelector(".team-container");
 //Eseguo ciclo per la lunghezza dell'array dove i = oggetto
 for( let i = 0; i < members.length; i++ ) {
-    console.log(members[i]);
     //Creo variabile a cui assegno il markup HTML inserendo le info ottenute ciclando l'array, quindi gli oggetti.
     const member = 
         `<div class="team-card">
             <div class="card-image">
                 <img
                     src="${members[i].memberPic}"
-                    alt="Wayne Barnett"
+                    alt="${members[i].memberName}"
                 />
             </div>
             <div class="card-text">
@@ -53,9 +52,47 @@ for( let i = 0; i < members.length; i++ ) {
                 <p>${members[i].role}</p>
             </div>
         </div>`;
-    console.log(members);
+    console.log(members, members[i]);
     //Stampo nel DOM, ad ogni ciclo, l'elemento presente più il nuovo.
     mainContainer.innerHTML += member;
 }
 // BONUS:
 // Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
+
+const newNameInput = document.getElementById("name");
+const newRoleInput = document.getElementById("role");
+const newPicInput = document.getElementById("image");
+const btn = document.querySelector("#addMemberButton");
+//Creo funzione in cui dato un oggetto e un sito, stampi il mark-up html con le info dell'oggetto e nel sito passati come parametri.
+function addCard(obj, to) {
+    const  newMember = 
+    `<div class="team-card">
+        <div class="card-image">
+            <img
+                src="${obj.memberPic}"
+                alt="${obj.memberName}"
+                />
+            </div>
+        <div class="card-text">
+            <h3>${obj.memberName}</h3>
+            <p>${obj.role}</p>
+        </div>
+    </div>`;
+    to.innerHTML += newMember;
+}
+//al click su btn creo un oggetto con i dati inseriti dall'utente
+btn.addEventListener("click", function(){
+    const newMembers = {
+        memberName: newNameInput.value,
+        role: newRoleInput.value,
+        memberPic: newPicInput.value,
+    };
+    //aggiungo l'oggetto all'array
+    members.push(newMembers);
+    //chiamo la funzione(obj, to)
+    addCard(newMembers, mainContainer);
+    //pulisco i valori degli input
+    newNameInput.value = "";
+    newRoleInput.value = "";
+    newPicInput.value = "";
+});
